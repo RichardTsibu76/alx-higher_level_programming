96a20cdd1ad1b20d17704b1a524f0954a02dbbc8
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-11-29 01:31:56 +0100

    task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,7 +7,7 @@ else:
     import math
     last_digit = math.fmod(number, 10)
 if (last_digit > 5):
-    print(f"Last digit of {number} is {last_digit} and is greater than 5")
+    print("Last digit of {} is {} and is greater than 5".foramt("number", "last_digit"))
 if (last_digit == 0):
     print(f"Last digit of {number} is {last_digit} and is 0")
 elif (last_digit != 0 and last_digit < 6):
