2882faa3e30a4d8613c3e06684a17842b825cfb9
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-11-30 22:20:18 +0100

    who are you

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import hidden_4
+
+if __name__ == "__main__":
+
+    absorber = dir(hidden_4)
+
+    number = len(absorber)
+
+    for integer in range(0, num):
+
+        if absorber[integer].startswith('_'):
+
+            continue
+
+        else:
+
+            print("{}".format(absober[integr]))
