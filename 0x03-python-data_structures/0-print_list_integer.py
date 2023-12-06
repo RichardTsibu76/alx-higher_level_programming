c689858f8e1cc4cfc5bc2548d24ee314ee2b1804
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-12-06 23:08:28 +0100

    update

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for integer_to_loop_through in my_list:
-        print("{:d}".format(integer_to_loop_through)
+        print("{:d}".format(integer_to_loop_through))
