c8fe401fee1dcd5b0f49c12f71a15f6d73814ad6
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-12-06 15:48:13 +0100

    task one

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-     num = len(my_list)
+    num = len(my_list)
     if idx < 0 or idx > (num - 1):
         return None
-    for number in range(0, num):
-        if number == idx:
+    for i in range(0, num):
+        if i == idx:
             return (my_list[idx])
