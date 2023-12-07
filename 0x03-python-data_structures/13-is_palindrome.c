374dcc55f386dac9869f4f1d1b9d7e00f2f754b3
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-12-07 16:25:25 +0100

    updated

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 
 /**
- * find_middle - Finds the middle node of a linked list.
- * @head: Pointer to the head of the linked list.
- * Return: Pointer to the middle node.
+ * find_middle - checker of middle node
+ * @head: This points to the first node
+ * Return: This returns to the middle of the node
  */
 listint_t *find_middle(listint_t *head)
 {
@@ -21,9 +21,9 @@ listint_t *find_middle(listint_t *head)
 }
 
 /**
- * reverse_list - Reverses a linked list.
- * @head: Pointer to the head of the linked list.
- * Return: Pointer to the new head of the reversed list.
+ * reverse_list - This traverses in reverse
+ * @head: The head points to the first node
+ * Return: Returns to the newhead after traversing
  */
 listint_t *reverse_list(listint_t *head)
 {
@@ -41,9 +41,9 @@ listint_t *reverse_list(listint_t *head)
 }
 
 /**
- * is_palindrome - Checks if a singly linked list is a palindrome.
- * @head: Pointer to the head of the linked list.
- * Return: 1 if palindrome, 0 if not.
+ * is_palindrome - This checks if singly linked list is a palindrome.
+ * @head: Points to the first node
+ * Return: This returns  1 if palindrome, otherwise 0 if not.
  */
 int is_palindrome(listint_t **head)
 {
