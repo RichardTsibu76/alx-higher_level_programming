416db9925a67058a6a012c10a20670c97b9df949
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-12-07 01:45:03 +0100

    tsk five

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element):
-    if idx < 0 or len(my_list) <= idx:
-        return my_list
-    else:
-        copy = my_list.copy()
-        copy[idx] = element
-        return copy
+def no_c(my_string):
+    if not my_string:
+        return ""
+    num = len(my_string)
+    idx = 0
+    while idx < num:
+        if idx == 0 and my_string[idx] != 'c' and my_string[idx] != 'C':
+            new_str = my_string[idx]
+        elif idx == 0 and (my_string[idx] == 'c' or my_string[idx] == 'C'):
+            new_str = ""
+        elif my_string[idx] == 'c' or my_string[idx] == 'C':
+            pass
+        # new_str += new_str[len(new_str):] + ""
+        else:
+            # new_str += new_str[len(new_str):] + my_string[idx]
+            new_str += my_string[idx]
+        idx = idx + 1
+    return new_str
