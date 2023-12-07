04b859519d585f8e8bc96829e16a96597f8a4f00
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-12-07 02:09:22 +0100

    task nine

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,4 +1,6 @@
- if my_list:
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
         maximum = None
         for number in my_list:
             if maximum is None or number > maximum:
