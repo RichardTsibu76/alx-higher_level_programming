b8aa7536db60ee15cec85a82a1d11d8bfd5b5353
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-12-07 16:17:45 +0100

    task 13

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 #include <Python.h>
 /**
- * print_python_list_info - Prints information about python list in c lang.
- * @p: PyListObject
+ * print_python_list_info - A C function that prints some
+ *                         basic info about Python lists
+ * @p: A PyObject to be type-cast to PyListObject
  */
 
 
 void print_python_list_info(PyObject *p)
 {
-	long int integer_i = 0;
+	long int i = 0;
 	PyListObject *my_list;
 
 	my_list = (PyListObject *)p;
 	printf("[*] Size of the Python List = %ld\n", my_list->ob_base.ob_size);
 	printf("[*] Allocated = %ld\n", my_list->allocated);
 
-	while (integer_i < my_list->ob_base.ob_size)
+	while (i < my_list->ob_base.ob_size)
 	{
-		printf("Element %ld: %s\n", integer_i,
-				my_list->ob_item[integer_i]->ob_type->tp_name);
+		printf("Element %ld: %s\n", i,
+				my_list->ob_item[i]->ob_type->tp_name);
 		i++;
 	}
 }
--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,36 +1,70 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
- * is_palindrome - checks for palindrom
- * @head: Points to the first node
- * Return: returns
+ * find_middle - Finds the middle node of a linked list.
+ * @head: Pointer to the head of the linked list.
+ * Return: Pointer to the middle node.
  */
-
-
-int is_palindrome(listint_t **head)
+listint_t *find_middle(listint_t *head)
 {
-	listint_t *first, *last, *current = *head;
-	int elements = 1, idx, half;
+	listint_t *slow = head, *fast = head;
 
-	for (; *head != NULL && current->next != NULL; elements++)
-		current = current->next;
-	if (*head == NULL || elements == 1)
-		return (1);
-	first = *head;
-	last = current;
-	half = (elements / 2) - 1;
-
-	while (((first->n) == (last->n)) && half > 0)
+	while (fast != NULL && fast->next != NULL)
 	{
-		first = first->next;
-		last = *head;
-		elements--;
-		half--;
-
-		for (idx = 1; idx < elements; idx++)
-			last = last->next;
+		fast = fast->next->next;
+		slow = slow->next;
 	}
 
-	if ((half == 0) && ((first->n) == (last->n)))
-		return (1);
-	return (0);
+	return (slow);
+}
+
+/**
+ * reverse_list - Reverses a linked list.
+ * @head: Pointer to the head of the linked list.
+ * Return: Pointer to the new head of the reversed list.
+ */
+listint_t *reverse_list(listint_t *head)
+{
+	listint_t *prev = NULL, *current = head, *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+
+	return (prev);
+}
+
+/**
+ * is_palindrome - Checks if a singly linked list is a palindrome.
+ * @head: Pointer to the head of the linked list.
+ * Return: 1 if palindrome, 0 if not.
+ */
+int is_palindrome(listint_t **head)
+{
+	int is_palindrome = 1;
+	listint_t *middle = find_middle(*head);
+	listint_t *second_half = reverse_list(middle);
+
+	listint_t *temp1 = *head, *temp2 = second_half;
+
+	while (temp1 != NULL && temp2 != NULL)
+	{
+		if (temp1->n != temp2->n)
+		{
+			is_palindrome = 0;
+			break;
+		}
+		temp1 = temp1->next;
+		temp2 = temp2->next;
+	}
+
+	reverse_list(second_half);
+
+	return (is_palindrome);
 }
