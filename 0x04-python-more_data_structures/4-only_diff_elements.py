9ca153ded0971e49da4b9e7c305725b9274cc3f1
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-12-10 02:11:59 +0100

    returns exclusively

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-#uing the symmwtric theory the smybol does it
     return set_1 ^ set_2
+# uing the symmetric theory smybol
+# This joins the element exclusively available in each set
