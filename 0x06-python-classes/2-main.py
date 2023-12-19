5ce789690a8580109a86c5de1af57fc28cdc3f09
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-12-20 00:49:54 +0100

    task two

--- 0x06-python-classes/2-main.py
+++ 0x06-python-classes/2-main.py
@@ -1,29 +1,34 @@
 #!/usr/bin/python3
-'''a defined class Square which takes an int or nothing as input'''
+Square = __import__('2-square').Square
 
+my_square_1 = Square(3)
+print(type(my_square_1))
+print(my_square_1.__dict__)
 
-class Square:
-    '''The class defines a Square'''
-    def __init__(self, size=0):
-        '''The initilization method takes an integer or nothing
-        Args:
-            size(int): An integer
-        Attributes:
-            self.__size: A private attibute named size
-        '''
-        self.set_size(size)
+my_square_2 = Square()
+print(type(my_square_2))
+print(my_square_2.__dict__)
 
-    def set_size(self, size):
-        '''The setter for self.__size
-        Args:
-            size(int): An integer or nothing
-        Attributes:
-            self.__size: A private attribute named size
-        '''
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-            return
-        if size < 0:
-            raise ValueError("size must be >= 0")
-            return
-        self.__size = size
+try:
+    print(my_square_1.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square_1.__size)
+except Exception as e:
+    print(e)
+
+try:
+    my_square_3 = Square("3")
+    print(type(my_square_3))
+    print(my_square_3.__dict__)
+except Exception as e:
+    print(e)
+
+try:
+    my_square_4 = Square(-89)
+    print(type(my_square_4))
+    print(my_square_4.__dict__)
+except Exception as e:
+    print(e)
