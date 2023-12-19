4debddd8bf2a012b700e08fe23f4fceaf74ef162
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-12-20 00:35:26 +0100

    task one

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''This is a class square that defines a square '''
+
+
+class Square:
+    '''This is actually a class Square
+    With private instance attribute of size'''
+
+
+    def __init__(self, size):
+        '''initialization method with parameter args size'''
+    self.__size = size
+    
+
