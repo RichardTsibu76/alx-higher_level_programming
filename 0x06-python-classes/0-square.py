6467ca3eb840bd3fd3716eeb177f4dad155021bb
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-12-20 00:19:58 +0100

    object oriented programming

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-'''Python module in oop containing an empty class that defines a square'''
+"""Python module in oop containing an empty class that defines a square"""
 
 class Square:
-    '''empty class'''
+    """empty class square"""
     pass
