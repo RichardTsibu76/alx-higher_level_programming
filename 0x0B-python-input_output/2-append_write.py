8d1ae8894ce4bd6c2c747ef0b50c93a2d1c9332c
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2024-01-09 23:08:02 +0100

    task two appending a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Appends a string at the end of a text file
+   using the UTF8 unicode
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    Appends a string at the end of a text file
+    Args:
+        filename (str): name of a text file
+        text (str): contents to append
+    Return:
+        the number of characters added
+    """
+    with open(filename, "a") as file_object:
+        return file_object.write(text)
