f3c4b2ecfd95164cbfd97d917014c0bb9ad08c52
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2024-01-09 23:23:43 +0100

    task seven of file_i/o_in_python

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Script to all arguments to a list and save them to a file"""
+
+
+from sys import argv
+save_to_json = __import__('5-save_to_json_file').save_to_json_file
+load_from_json = __import__('6-load_from_json_file').load_from_json_file
+filename = "add_item.json"
+try:
+    arg_list = load_from_json(filename)
+except FileNotFoundError:
+    arg_list = []
+arg_list.extend(argv[1:])
+save_to_json(arg_list, filename)
