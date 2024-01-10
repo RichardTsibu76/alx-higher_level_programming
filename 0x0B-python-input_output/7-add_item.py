f824acb3af8393159a5fdb2cc1175144e24560f5
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2024-01-10 04:50:36 +0100

    task nine

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,14 +1,30 @@
 #!/usr/bin/python3
-"""Script to all arguments to a list and save them to a file"""
-
-
+"""JSON rep object addition module"""
 from sys import argv
-save_to_json = __import__('5-save_to_json_file').save_to_json_file
-load_from_json = __import__('6-load_from_json_file').load_from_json_file
-filename = "add_item.json"
-try:
-    arg_list = load_from_json(filename)
-except FileNotFoundError:
-    arg_list = []
-arg_list.extend(argv[1:])
-save_to_json(arg_list, filename)
+import os
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+def add_arguments_to_list(arguments):
+    """a function that adds arguments to list"""
+
+    if os.path.exists("add_item.json"):
+        existing_list = load_from_json_file("add_item.json")
+    else:
+        existing_list = []
+    existing_list.extend(arguments)
+    return existing_list
+
+
+if __name__ == "__main__":
+    # Get command-line arguments (excluding the script name)
+    arguments = argv[1:]
+    if arguments:
+        updated_list = add_arguments_to_list(arguments)
+        save_to_json_file(updated_list, "add_item.json")
+#       print("Arguments added to 'add_item.json'.")
+#   else:
+#        print("No arguments provided.")
