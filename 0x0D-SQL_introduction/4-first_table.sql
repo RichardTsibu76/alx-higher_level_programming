8dca88f7d68d0e33be15a81614d8a539fa9bc333
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2024-02-14 02:12:02 +0100

    update all the files

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,9 @@
+-- a script that creates a table called firt_table
+-- the current database in your MySQL server
+-- first_table description
+-- id INT
+-- name VARCHAR(256)
+-- The table nane would be passed as args
+-- SELECT and show are not be used
+
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
