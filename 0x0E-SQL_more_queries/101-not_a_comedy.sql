65dec0cf5a462d859de8b4ccef583249269c204a
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2024-02-15 04:52:48 +0100

    101-not_a_comedy.sql

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,5 +1,3 @@
--- lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
--- The tv_genres table contains only one record where name = Comedy (but the id can be different)
 -- Each record should display: tv_shows.title
 -- Results must be sorted in ascending order by the show title
 -- You can use a maximum of two SELECT statement
