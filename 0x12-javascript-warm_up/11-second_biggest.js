fdc48545ed1e9837a13732ea78ed299064947ac5
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2024-03-11 23:01:50 +0100

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,9 +2,9 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-    const ARGS = process.argv
-      .map(Number)
-      .slice(2, process.argv.length)
-      .sort((a, b) => a - b);
-    console.log(ARGS[ARGS.length - 2]);
+  const ARGS = process.argv
+    .map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(ARGS[ARGS.length - 2]);
 }
