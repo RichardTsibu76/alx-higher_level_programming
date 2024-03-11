4a97ef89a7d61057879ed02c74e739e6400263ee
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2024-03-11 15:05:10 +0100

    task 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -3,5 +3,5 @@
 /*
  *Printing all at once using the console.log
  */
-const va_DISPLAY = 'C is fun\nPython is cool\nJavaScript is amazing';
-console.log(va_DISPLAY);
+const DISPLAY = 'C is fun\nPython is cool\nJavaScript is amazing';
+console.log(DISPLAY);
