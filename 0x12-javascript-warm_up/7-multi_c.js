238fa17f46dc421dab346eb061a958ad1e93d6ee
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2024-03-11 19:05:59 +0100

    task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,8 @@ const x = process.argv[2];
 if (!parseInt(x)) {
 console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < x; i++) {
-    console.log('C is fun');
-  }
+for (let i = 0; i < x; i++)
+{
+console.log('C is fun');
+}
 }
