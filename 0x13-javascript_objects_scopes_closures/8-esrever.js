787059a465169b37e9cdc234a0ffd59513af73b5
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2024-03-12 22:55:48 +0100

    task eight

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 
 exports.esrever = function (list) {
   const reversedList = []; // The empty list to be used using the push
-  for (let i = list.length - 1; i >= 0; i--) {
+  for (let n = list.length - 1; n >= 0; n--) {
     reversedList.push(list[i]);
   }
 
