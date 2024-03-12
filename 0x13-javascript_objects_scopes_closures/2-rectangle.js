54b805a164d78e1c4015f6a601f88a716d0e0cbc
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2024-03-12 22:03:14 +0100

    tsk two

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
-//Defines a class
+/**
+ *Check the parameters provided
+ */
 class Rectangle {
   constructor (w, h) {
     if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
