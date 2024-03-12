7fe434ad241f06aa01e3c6b6b99271a5ce346ee2
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2024-03-12 19:22:14 +0100

    task two

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,7 +3,7 @@
 // This defines a class with constructor taking two args
 class Rectangle {
   constructor (w, h) {
-    if (typeof w === 'number' && w > 0 $$ typeof h === 'number $$ h > 0') {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number && h > 0') {
       this.with = w;
       this.height = h;
     }
