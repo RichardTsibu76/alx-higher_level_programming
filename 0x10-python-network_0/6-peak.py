9bcb775c7751bef2d53e4e8fe8c0ae514c341c39
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2024-03-28 01:25:50 +0100

    TASK UPDATED

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,30 +1,16 @@
-#!/usr/bin/pyhton3
-"Finding"
+#!/usr/bin/python3
+""" module doc """
+
 
 def find_peak(list_of_integers):
-    '''
-    Args:
-        list_of_integers(int): list of integers to find peak of
-    Returns: peak of list_of_integers or None
-    '''
-    size = len(list_of_integers)
-    mid_e = size
-    mid = size // 2
-
-    if size == 0:
-        return None
-
-    while True:
-        mid_e = mid_e // 2
-        if (mid < size - 1 and 
-                list_of_integers[mid] < list_of_integers[mid + 1]):
-            if mid_e // 2 == 0:
-                mid_e = 2
-            mid = mid + mid_e // 2
-        elif mid_e > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-            if mid_e // 2 == 0:
-                mid_e = 2
-            mid = mid  - mid_e // 2
-        else
-            return list_of_integers[mid]
-
+    """def doc"""
+    if list_of_integers:
+        lf = 0
+        r = len(list_of_integers) - 1
+        while lf < r:
+            m = (lf + r) // 2
+            if list_of_integers[m] > list_of_integers[m + 1]:
+                r = m
+            else:
+                lf = m + 1
+        return list_of_integers[lf]
